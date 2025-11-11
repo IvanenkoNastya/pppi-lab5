3b5829753bcb31157efb5e21fe5045a35adc4551
IvanenkoNastya <ivanenko.nastya.a@gmail.com>
2025-11-11 17:01:06 +0200

    Add class and method declarations

---
+++ pppi-lab5-calculator/pppi-lab5-calculator/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pppi_lab5_calculator {
+	internal class Calculator {
+		public float Add(float n, float m) {
+			float result = 0;
+			// to do
+			return result;
+		}
+
+		public float Substract(float n, float m) {
+			float result = 0;
+			// to do
+			return result;
+		}
+
+		public float Multiply(float n, float m) {
+			float result = 0;
+			// to do
+			return result;
+		}
+
+		public float Divide(float n, float m) {
+			float result = 0;
+			// to do
+			return result;
+		}
+
+		public float Pow(float n, float m) {
+			float result = 0;
+			// to do
+			return result;
+		}
+	}
+}
---
+++ pppi-lab5-calculator/pppi-lab5-calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pppi_lab5_calculator {
+	internal class Program {
+		static void Main(string[] args) {
+			Calculator calculator = new Calculator();
+
+
+
+		}
+	}
+}
